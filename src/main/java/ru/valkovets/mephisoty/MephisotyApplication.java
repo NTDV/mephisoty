eb7a597785fe74555154a47972f5bdbf9b0e84a2
Данила Вальковец <dahnh33@gmail.com>
2024-06-19 23:28:01 +0300

    Initial

---
+++ src/main/java/ru/valkovets/mephisoty/MephisotyApplication.java
@@ -0,0 +1,13 @@
+package ru.valkovets.mephisoty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MephisotyApplication {
+
+public static void main(String[] args) {
+    SpringApplication.run(MephisotyApplication.class, args);
+}
+
+}
---
+++ src/test/java/ru/valkovets/mephisoty/MephisotyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.valkovets.mephisoty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MephisotyApplicationTests {
+
+@Test
+void contextLoads() {
+}
+
+}
