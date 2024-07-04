be062ae5534ae933581cba8fd585d2ba7ca62789
NTDV <dahnh33@gmail.com>
2024-07-04 03:14:27 +0300

    Add front config

--- front/vite.config.js
+++ front/vite.config.js
@@ -6,6 +6,11 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
     return {
+        esbuild: {
+            supported: {
+                'top-level-await': true
+            },
+        },
         plugins: [vue()],
         resolve: {
             alias: {
